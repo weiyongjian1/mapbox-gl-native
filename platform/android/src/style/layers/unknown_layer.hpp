d102e5d3034f7362cb39c3f43529dc777a04ffe9
Ivo van Dongen <info@ivovandongen.nl>
2017-02-24 16:06:09 +0200

    [android] add unknown layer type

---
+++ platform/android/src/style/layers/unknown_layer.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "layer.hpp"
+#include <mbgl/style/layer.hpp>
+#include <jni/jni.hpp>
+
+namespace mbgl {
+namespace android {
+
+class UnknownLayer : public Layer {
+public:
+
+    static constexpr auto Name() { return "com/mapbox/mapboxsdk/style/layers/UnknownLayer"; };
+
+    static jni::Class<UnknownLayer> javaClass;
+
+    static void registerNative(jni::JNIEnv&);
+
+    UnknownLayer(mbgl::Map&, mbgl::style::Layer&);
+
+    ~UnknownLayer() = default;
+
+    jni::jobject* createJavaPeer(jni::JNIEnv&);
+
+}; // class UnknownLayer
+
+} // namespace android
+} // namespace mbgl
